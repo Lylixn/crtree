fc53c7fd441974a69e7ddab72d9e21e514c3426d
l-guerra-rago <lylian.gr@protonmail.com>
2024-02-01 16:25:04 +0100

    WIP: one more bug and its done

--- src/reader/reader.cr
+++ src/reader/reader.cr
@@ -14,15 +14,17 @@ class Reader
     puts "\n  \033[1;32m#{@@files} files\033[0m, \033[1;34m#{@@directories} directories\033[0m"
   end
 
-  def read(path : String | Path, level = 0)
+  def read(path : String | Path, level : Number = 0, last : Bool = false)
     directory = Dir.new(path)
     prefix = "│    " * level
 
     directory.each_child do |element|
       decorator = "├─"
-      # if element == directory.children.last
-      #   decorator = "└─"
-      # end
+      if element == Dir.new(path).children.last
+        decorator = "└─"
+      else
+        decorator = "├─"
+      end
 
       if File.file?("#{path}/#{element}")
         # check if file is executable
@@ -40,7 +42,7 @@ class Reader
 
         @@directories += 1
         puts prefix + decorator + "─ \033[0;34m" + element + "\033[0m"
-        read(directory.path + '/' + element, level + 1)
+        read(directory.path + '/' + element, level + 1, Dir.new(path).children.last == element)
       end
 
       next
