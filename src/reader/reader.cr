5c028fe5ebf3c5f344b48fe9b8f4dc74efac179c
l-guerra-rago <lylian.gr@protonmail.com>
2024-02-01 14:15:36 +0100

    WIP: all MVP work but not for the last element decorator

--- src/reader/reader.cr
+++ src/reader/reader.cr
@@ -6,19 +6,37 @@ class Reader
     @path = path
     @directory = Dir.new(@path)
 
-    read(@directory)
+    puts "."
+    read(@path)
   end
 
-  def read(directory : Dir)
-    directory.each_child do |element|
-      puts File.file?(element)
-      if File.file?(element)
-        puts element
-        next
-      end
+  def read(path : String | Path, level = 0)
+    directory = Dir.new(path)
+    prefix = "│    " * level
 
-      puts "Directory: #{element}"
-      read(Dir.new(directory.path + '/' + element))
+    directory.each_child do |element|
+      decorator = "├─"
+      # if element == directory.children.last
+      #   decorator = "└─"
+      # end
+
+      if File.file?("#{path}/#{element}")
+        # check if file is executable
+        if File.executable?("#{path}/#{element}")
+          puts prefix + decorator + "─ \033[1;32m" + element + "\033[0m"
+        else
+          puts prefix + decorator + "─ " + element
+        end
+
+      else File.directory?("#{path}/#{element}")
+        if element == ".git" || element == "node_modules" || element == "." || element == ".." || element == ".idea"
+          next
+        end
+
+        puts prefix + decorator + "─ \033[0;34m" + element + "\033[0m"
+        read(directory.path + '/' + element, level + 1)
+      end
+      next
     end
   end
 
