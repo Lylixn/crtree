74c0968aa9deeede1126999d3085c238e2005c1b
l-guerra-rago <lylian.gr@protonmail.com>
2024-02-01 14:29:07 +0100

    WIP: add files and folder count

--- src/reader/reader.cr
+++ src/reader/reader.cr
@@ -1,13 +1,16 @@
 class Reader
   property path : String | Path
   property directory : Dir
+  @@files : Int32 = 0
+  @@directories : Int32 = 0
 
   def initialize (path : String | Path)
     @path = path
     @directory = Dir.new(@path)
 
-    puts "."
+    puts "\033[0;34m.\033[0m"
     read(@path)
+    puts "\n  \033[1;32m#{@@files} files\033[0m, \033[1;34m#{@@directories} directories\033[0m"
   end
 
   def read(path : String | Path, level = 0)
@@ -27,15 +30,18 @@ class Reader
         else
           puts prefix + decorator + "─ " + element
         end
+        @@files += 1
 
       else File.directory?("#{path}/#{element}")
         if element == ".git" || element == "node_modules" || element == "." || element == ".." || element == ".idea"
           next
         end
 
+        @@directories += 1
         puts prefix + decorator + "─ \033[0;34m" + element + "\033[0m"
         read(directory.path + '/' + element, level + 1)
       end
+
       next
     end
   end
