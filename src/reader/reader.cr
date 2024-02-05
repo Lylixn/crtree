d56b47c3f757ba973b03bc09aed6ba19289f9f7e
lguerra--rago <lylian.gr@protonmail.com>
2024-02-05 13:00:40 +0100

    WIP

--- src/reader/reader.cr
+++ src/reader/reader.cr
@@ -13,20 +13,18 @@ class Reader
     puts "\n  \033[1;32m#{@@files} files\033[0m, \033[1;34m#{@@directories} directories\033[0m"
   end
 
-  def read(path : String | Path, level : Number = 0, last : Bool = false)
+  def read(path : String | Path, level : Number = 0, last : Bool = false, prefix : String = "")
+
     directory = Dir.new(path)
-    prefix = "│    " * level
 
     directory.each_child do |element|
+
       decorator = "├─"
       if element == Dir.new(path).children.last
         decorator = "└─"
-      else
-        decorator = "├─"
       end
 
       if File.file?("#{path}/#{element}")
-        # check if file is executable
         if File.executable?("#{path}/#{element}")
           puts prefix + decorator + "─ \033[1;32m" + element + "\033[0m"
         else
@@ -35,10 +33,9 @@ class Reader
         @@files += 1
 
       else File.directory?("#{path}/#{element}")
-
         @@directories += 1
         puts prefix + decorator + "─ \033[0;34m" + element + "\033[0m"
-        read(directory.path + '/' + element, level + 1, Dir.new(path).children.last == element)
+        read(directory.path + '/' + element, level + 1, Dir.new(path).children.last == element, prefix + (element == Dir.new(path).children.last ? "    " : "│   "))
       end
 
       next
